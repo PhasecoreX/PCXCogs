b47751af9b2262a1e47e1e2a4cf13e0966b779a7
Ryan Foster <phasecorex@gmail.com>
2020-10-20 18:44:28 -0400

    [Wikipedia] Add menu system and case insensitive search
    Also add typing indicator while doing lookup and creating embeds

--- wikipedia/wikipedia.py
+++ wikipedia/wikipedia.py
@@ -4,19 +4,13 @@ import discord
 from redbot.core import __version__ as redbot_version
 from redbot.core import commands
 from redbot.core.utils.chat_formatting import error, warning
+from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
 
 __author__ = "PhasecoreX"
 
 
 class Wikipedia(commands.Cog):
     """Look up stuff on Wikipedia."""
-
-    base_url = "https://en.wikipedia.org/w/api.php"
-    headers = {"user-agent": "Red-DiscordBot/" + redbot_version}
-    footer_icon = (
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Wikimedia-logo.png"
-        "/600px-Wikimedia-logo.png"
-    )
 
     async def red_delete_data_for_user(self, **kwargs):
         """Nothing to delete."""
@@ -25,51 +19,45 @@ class Wikipedia(commands.Cog):
     @commands.command(aliases=["wiki"])
     async def wikipedia(self, ctx: commands.Context, *, query: str):
         """Get information from Wikipedia."""
-        payload = self.generate_payload(query)
-        conn = aiohttp.TCPConnector()
-        async with aiohttp.ClientSession(connector=conn) as session:
-            async with session.get(
-                self.base_url, params=payload, headers=self.headers
-            ) as res:
-                result = await res.json()
+        async with ctx.typing():
+            payload = self.generate_payload(query)
+            conn = aiohttp.TCPConnector()
+            async with aiohttp.ClientSession(connector=conn) as session:
+                async with session.get(
+                    "https://en.wikipedia.org/w/api.php",
+                    params=payload,
+                    headers={"user-agent": "Red-DiscordBot/" + redbot_version},
+                ) as res:
+                    result = await res.json()
 
-        try:
-            # Get the last page. Usually this is the only page.
-            for page in result["query"]["pages"]:
-                title = page["title"]
-                description = page["extract"].strip().replace("\n", "\n\n")
-                image = (
-                    page["original"]["source"]
-                    if "original" in page and "source" in page["original"]
-                    else None
-                )
-                url = page["fullurl"]
+            embeds = []
+            if "query" in result and "pages" in result["query"]:
+                for page in result["query"]["pages"]:
+                    try:
+                        embeds.append(self.generate_embed(page))
+                        if not ctx.channel.permissions_for(ctx.me).embed_links:
+                            # No embeds here :(
+                            await ctx.send(
+                                warning(
+                                    "I'm not allowed to do embeds here...\n{}".format(
+                                        page["fullurl"]
+                                    )
+                                )
+                            )
+                            return
+                        if not ctx.channel.permissions_for(ctx.me).add_reactions:
+                            break  # Menu can't function so only show first result
+                    except KeyError:
+                        pass
 
-            if len(description) > 1500:
-                description = description[:1500].strip()
-                description += "... [(read more)]({})".format(url)
-
-            embed = discord.Embed(
-                title="Wikipedia: {}".format(title),
-                description=u"\u2063\n{}\n\u2063".format(description),
-                color=discord.Color.blue(),
-                url=url,
-            )
-            if image:
-                embed.set_image(url=image)
-            embed.set_footer(
-                text="Information provided by Wikimedia", icon_url=self.footer_icon
-            )
-            await ctx.send(embed=embed)
-
-        except KeyError:
+        if not embeds:
             await ctx.send(
                 error("I'm sorry, I couldn't find \"{}\" on Wikipedia".format(query))
             )
-        except discord.Forbidden:
-            await ctx.send(
-                warning("I'm not allowed to do embeds here...\n{}".format(url))
-            )
+        elif len(embeds) == 1:
+            await ctx.send(embed=embeds[0])
+        else:
+            await menu(ctx, embeds, DEFAULT_CONTROLS, timeout=60.0)
 
     @staticmethod
     def generate_payload(query: str):
@@ -83,9 +71,12 @@ class Wikipedia(commands.Cog):
         payload["formatversion"] = "2"  # Modern format
 
         # action:query options
-        payload["titles"] = query.replace(" ", "_")  # A list of titles to work on
+        payload["generator"] = "search"  # Get list of pages by executing a query module
         payload["redirects"] = "1"  # Automatically resolve redirects
         payload["prop"] = "extracts|info|pageimages"  # Which properties to get
+
+        # action:query/generator:search options
+        payload["gsrsearch"] = "intitle:{}".format(query)  # Search for page titles
 
         # action:query/prop:extracts options
         payload["exintro"] = "1"  # Return only content before the first section
@@ -97,3 +88,36 @@ class Wikipedia(commands.Cog):
         # action:query/prop:pageimages options
         payload["piprop"] = "original"  # Return URL of page image, if any
         return payload
+
+    @staticmethod
+    def generate_embed(page_json):
+        """Generate the embed for the json page."""
+        title = page_json["title"]
+        description = page_json["extract"].strip().replace("\n", "\n\n")
+        image = (
+            page_json["original"]["source"]
+            if "original" in page_json and "source" in page_json["original"]
+            else None
+        )
+        url = page_json["fullurl"]
+
+        if len(description) > 1500:
+            description = description[:1500].strip()
+            description += "... [(read more)]({})".format(url)
+
+        embed = discord.Embed(
+            title="Wikipedia: {}".format(title),
+            description=u"\u2063\n{}\n\u2063".format(description),
+            color=discord.Color.blue(),
+            url=url,
+        )
+        if image:
+            embed.set_image(url=image)
+        embed.set_footer(
+            text="Information provided by Wikimedia",
+            icon_url=(
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Wikimedia-logo.png"
+                "/600px-Wikimedia-logo.png"
+            ),
+        )
+        return embed
