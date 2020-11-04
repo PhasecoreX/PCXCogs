3c8dbb2767f28e00c1550e3a7f3551887200a49a
Ryan Foster <phasecorex@gmail.com>
2020-11-04 18:21:05 -0500

    [UwU] ( ˘⌣˘)♡(˘⌣˘ )
    p-pwease don't wook at this commit (o^▽^o)

--- uwu/uwu.py
+++ uwu/uwu.py
@@ -1,4 +1,6 @@
 """UwU cog for Red-DiscordBot by PhasecoreX."""
+import random
+
 import discord
 from redbot.core import commands
 
@@ -10,62 +12,129 @@ __author__ = "PhasecoreX"
 class UwU(commands.Cog):
     """UwU."""
 
+    KAOMOJI_JOY = [
+        " (* ^ ω ^)",
+        " (o^▽^o)",
+        " (≧◡≦)",
+        ' ☆⌒ヽ(*"､^*)chu',
+        " ( ˘⌣˘)♡(˘⌣˘ )",
+        " xD",
+    ]
+    KAOMOJI_EMBARRASSED = [
+        " (⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄)..",
+        " (*^.^*)..,",
+        "..,",
+        ",,,",
+        "... ",
+        ".. ",
+        " mmm..",
+        "O.o",
+    ]
+    KAOMOJI_CONFUSE = [" (o_O)?", " (°ロ°) !?", " (ーー;)?", " owo?"]
+    KAOMOJI_SPARKLES = [" *:･ﾟ✧*:･ﾟ✧ ", " ☆*:・ﾟ ", "〜☆ ", " uguu.., ", "-.-"]
+
     async def red_delete_data_for_user(self, **kwargs):
         """Nothing to delete."""
         return
 
     @commands.command(aliases=["owo"])
-    async def uwu(self, ctx: commands.Context):
-        """Uwuize the pwevious comment."""
-        message = (await ctx.channel.history(limit=2).flatten())[1].content
-        if not message:
-            message = "I can't translate that!"
+    async def uwu(self, ctx: commands.Context, *, text: str = None):
+        """Uwuize the pwevious message, ow youw own text."""
+        if not text:
+            text = (await ctx.channel.history(limit=2).flatten())[
+                1
+            ].content or "I can't translate that!"
         await type_message(
             ctx.channel,
-            self.uwuize_string(message),
+            self.uwuize_string(text),
             allowed_mentions=discord.AllowedMentions(
                 everyone=False, users=False, roles=False
             ),
         )
 
-    @staticmethod
-    def uwuize_string(string: str):
+    def uwuize_string(self, string: str):
         """Uwuize and wetuwn a stwing."""
         converted = ""
-
-        def uwuize_word(string: str):
-            """Uwuize and wetuwn a wowd."""
-            # Whowe wowds
-            if string.lower().startswith("this") or string.lower().startswith("that"):
-                dee = "D" if string[0].isupper() else "d"
-                string = dee + string[2:]
-            else:
-                # Genewic wepwacements
-                if (
-                    string.lower().startswith("m")
-                    and len(string) > 1
-                    and string.lower() != "me"
-                ):
-                    doubleyou = "W" if string[1].isupper() else "w"
-                    string = string[0] + doubleyou + string[1:]
-                string = string.replace("r", "w").replace("R", "W")
-                string = string.replace("l", "w").replace("L", "W")
-            # Suffixes
-            if string.endswith("?"):
-                string = string + "? UwU"
-            if string.endswith("!"):
-                string = string + "! OwO"
-            return string
-
         current_word = ""
         for letter in string:
             if letter.isprintable() and not letter.isspace():
                 current_word += letter
             elif current_word:
-                converted += uwuize_word(current_word) + letter
+                converted += self.uwuize_word(current_word) + letter
                 current_word = ""
             else:
                 converted += letter
         if current_word:
-            converted += uwuize_word(current_word)
+            converted += self.uwuize_word(current_word)
         return converted
+
+    def uwuize_word(self, word: str):
+        """Uwuize and wetuwn a wowd.
+
+        Thank you to the following for inspiration:
+        https://github.com/senguyen1011/UwUinator
+        """
+        word = word.lower()
+        uwu = word.rstrip(".?!,")
+        punctuations = word[len(uwu) :]
+        final_punctuation = punctuations[-1] if punctuations else ""
+        extra_punctuation = punctuations[:-1] if punctuations else ""
+
+        # Process punctuation
+        if final_punctuation == "." and not random.randint(0, 3):
+            final_punctuation = random.choice(self.KAOMOJI_JOY)
+        if final_punctuation == "?" and not random.randint(0, 2):
+            final_punctuation = random.choice(self.KAOMOJI_CONFUSE)
+        if final_punctuation == "!" and not random.randint(0, 2):
+            final_punctuation = random.choice(self.KAOMOJI_JOY)
+        if final_punctuation == "," and not random.randint(0, 3):
+            final_punctuation = random.choice(self.KAOMOJI_EMBARRASSED)
+        if final_punctuation and not random.randint(0, 4):
+            final_punctuation = random.choice(self.KAOMOJI_SPARKLES)
+
+        # L -> W and R -> W
+        protected = ""
+        if (
+            uwu.endswith("le")
+            or uwu.endswith("ll")
+            or uwu.endswith("er")
+            or uwu.endswith("re")
+        ):
+            protected = uwu[-2:]
+            uwu = uwu[:-2]
+        elif (
+            uwu.endswith("les")
+            or uwu.endswith("lls")
+            or uwu.endswith("ers")
+            or uwu.endswith("res")
+        ):
+            protected = uwu[-3:]
+            uwu = uwu[:-3]
+        uwu = uwu.replace("l", "w").replace("r", "w") + protected
+
+        # Full words
+        uwu = uwu.replace("you're", "ur")
+        uwu = uwu.replace("youre", "ur")
+        uwu = uwu.replace("fuck", "fwickk")
+        uwu = uwu.replace("shit", "poopoo")
+        uwu = uwu.replace("bitch", "meanie")
+        uwu = uwu.replace("asshole", "b-butthole")
+        uwu = uwu.replace("dick", "peenie")
+        uwu = uwu.replace("penis", "peenie")
+        uwu = "cummies" if uwu in ("cum", "semen") else uwu
+        uwu = "boi pussy" if uwu == "ass" else uwu
+        uwu = "daddy" if uwu in ("dad", "father") else uwu
+
+        # Add back punctuations
+        uwu += extra_punctuation + final_punctuation
+
+        # Add occasional stutter
+        if (
+            len(uwu) > 2
+            and uwu[0].isalpha()
+            and "-" not in uwu
+            and not random.randint(0, 6)
+        ):
+            uwu = "{}-{}".format(uwu[0], uwu)
+
+        return uwu
