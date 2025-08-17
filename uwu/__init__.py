218ab28ec7a1a096522283d468882f4f4a2d3565
DidiDidi129 <136057713+DidiDidi129@users.noreply.github.com>
2025-08-17 19:23:16 +0930

    a

--- uwu/__init__.py
+++ uwu/__init__.py
@@ -2,16 +2,12 @@
 
 import json
 from pathlib import Path
-
 from redbot.core.bot import Red
-
 from .uwu import UwU
 
 with Path(__file__).parent.joinpath("info.json").open() as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
-
 async def setup(bot: Red) -> None:
     """Load UwU cog."""
-    cog = UwU()
-    await bot.add_cog(cog)
+    await bot.add_cog(UwU(bot))  # ✅ pass bot here
